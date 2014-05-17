42ec0e7c5d219f3629fbfdfdc886ef504cd7a8f1
bstascavage <brian@stascavage.com>
2014-05-17 13:14:01 -0700

    added config file for camping server

--- camping/GroupStats.rb
+++ camping/GroupStats.rb
@@ -1,10 +1,15 @@
 require 'sqlite3'
 require 'json'
+require 'yaml'
 
 Camping.goes :GroupStats
+if !ARGV[1].nil?
+    $config_file = ARGV[1]
+else
+    $config_file = 'web.yaml'
+end
 
-$database = ARGV[1]
-
+pp $config_file
 module GroupStats::Controllers
   class Index < R '/'
     def get
@@ -17,11 +22,18 @@ module GroupStats::Controllers
 		if(@input.content == nil)
 			@input.content = ""
 		end
-		begin
-            database = SQLite3::Database.new( $database ) #name your db as db.sqlite3 and put it in the same folder as this file
+        begin
+            config = YAML.load_file($config_file)
+        rescue
+            abort('Configuration file not found.  Exiting...')
+        end
+ 
+        begin
+            database = SQLite3::Database.new( config['groupme']['database'] ) #name your db as db.sqlite3 and put it in the same folder as this file
         rescue
             abort('Did not specify a valid database file')
         end
+
 		result = database.execute( "SELECT users.Name, count(user_id) as count FROM messages join users on messages.user_id = users.Uid group by user_id order by count desc")
 		headers['Content-Type'] = "application/json"
 		return result.to_json
