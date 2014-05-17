1279a1d0a2e0c308ba37491a7c3d3ade83ad2d21
bstascavage <brian@stascavage.com>
2014-05-16 21:09:13 -0700

    Added felxibale database path

--- camping/GroupStats.rb
+++ camping/GroupStats.rb
@@ -3,6 +3,8 @@ require 'json'
 
 Camping.goes :GroupStats
 
+$database = ARGV[1]
+
 module GroupStats::Controllers
   class Index < R '/'
     def get
@@ -15,7 +17,11 @@ module GroupStats::Controllers
 		if(@input.content == nil)
 			@input.content = ""
 		end
-		database = SQLite3::Database.new( 'db.sqlite3' ) #name your db as db.sqlite3 and put it in the same folder as this file
+		begin
+            database = SQLite3::Database.new( $database ) #name your db as db.sqlite3 and put it in the same folder as this file
+        rescue
+            abort('Did not specify a valid database file')
+        end
 		result = database.execute( "SELECT users.Name, count(user_id) as count FROM messages join users on messages.user_id = users.Uid group by user_id order by count desc")
 		headers['Content-Type'] = "application/json"
 		return result.to_json
