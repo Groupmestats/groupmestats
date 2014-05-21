89816e693fb356ff1c3c5461a3c57d59347e31da
wdonnell <cfonix@gmail.com>
2014-05-21 15:57:47 -0400

    moving db connection to global var

--- camping/GroupStats.rb
+++ camping/GroupStats.rb
@@ -10,16 +10,32 @@ else
     $config_file = 'web.yaml'
 end
 
+def GroupStats.create
+	puts "creating db connection"
+	begin 
+		config = YAML.load_file($config_file)
+	rescue
+		abort('Configuration file not found.  Exiting...')
+	end
+
+	begin
+		$database = SQLite3::Database.new( config['groupme']['database'] ) #name your db as db.sqlite3 and put it in the same folder as this file
+	rescue
+		abort('Did not specify a valid database file')
+	end
+end
+
 module GroupStats::Controllers
   class Index < R '/'
     def get
-      begin 
-        config = YAML.load_file($config_file)
-      rescue
-        abort('Configuration file not found.  Exiting...')
-      end
       File.open('index.html')
     end
+  end
+  
+  class GroupList < R '/rest/grouplist'
+	def get()
+		return 'something'
+	end
   end
   
   class PostsMost < R '/rest/postsmost'
@@ -37,13 +53,9 @@ module GroupStats::Controllers
             abort('Configuration file not found.  Exiting...')
         end
  
-        begin
-            database = SQLite3::Database.new( config['groupme']['database'] ) #name your db as db.sqlite3 and put it in the same folder as this file
-        rescue
-            abort('Did not specify a valid database file')
-        end
+        
 
-		result = database.execute( "SELECT user_groups.Name, count(messages.user_id) as count
+		result = $database.execute( "SELECT user_groups.Name, count(messages.user_id) as count
 										FROM user_groups
 										join users on users.user_id = user_groups.user_id
 										join messages on messages.user_id = users.user_id
