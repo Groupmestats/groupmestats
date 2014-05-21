02761df84ddc8e93d9719ddce083b656979ddf98
bstascavage <brian@stascavage.com>
2014-05-21 15:53:55 -0700

    Rremoved HTTParty stuff from scraper

--- bin/scraper.rb
+++ bin/scraper.rb
@@ -6,42 +6,6 @@ require 'time'
 require 'sqlite3'
 require_relative 'groupme'
 
-#Class that invokes HTTParty. 
-class Groupme
-    include HTTParty
-
-    base_uri 'https://api.groupme.com/v3//'
-    format :json
-
-    def get(query, token, args=nil)
-        if args.nil?
-            new_query = query + "?token=#{token}"
-        else
-            new_query = query + "?token=#{token}&#{args}"
-        end
-
-        response = self.class.get(new_query, :verify => false)
-
-        retry_attempts = 0
-
-        if response.code != 200            
-            if response.nil?
-                abort('No more messages returned from groupme. Perhaps you have pulled all available messages?')
-            end
-            while retry_attempts < 3 do
-                puts "Could not connect to groupme.com.  Will retry in 60 seconds"       
-                sleep(60)
-                self.class.get(query)
-                retry_attempts += 1
-            end
-            if retry_attempts >= 3
-                abort('Could not connect to groupme')
-            end
-        end
-        return response
-    end
-end
-
 #Scraper class.  This is initialized with a path to the sqlite database and a groupme oauth token
 class Scraper
     $time = Time.now.to_i
