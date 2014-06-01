916885ce681e7ae564b9de3e959106f552f1e341
wdonnell <cfonix@gmail.com>
2014-06-01 15:34:08 -0400

    style

--- camping/GroupStats.rb
+++ camping/GroupStats.rb
@@ -11,38 +11,46 @@ else
 end
 
 def GroupStats.create
-	puts "creating db connection"
-	begin 
-		config = YAML.load_file($config_file)
-	rescue
-		abort('Configuration file not found.  Exiting...')
-	end
+    puts "creating db connection"
+    begin 
+        config = YAML.load_file($config_file)
+    rescue
+        abort('Configuration file not found.  Exiting...')
+    end
 
-	begin
-		$database = SQLite3::Database.new( config['groupme']['database'] ) #name your db as db.sqlite3 and put it in the same folder as this file
-	rescue
-		abort('Did not specify a valid database file')
-	end
+    begin
+        $database = SQLite3::Database.new( config['groupme']['database'] )
+    rescue
+        abort('Did not specify a valid database file')
+    end
 end
 
 module GroupStats::Controllers
+
   class Index < R '/'
     def get
-       begin 
-         config = YAML.load_file($config_file)
-       rescue
-         abort('Configuration file not found.  Exiting...')
-       end
-       File.open(File.join(File.expand_path(File.dirname(__FILE__)), 'index.html') )
+        begin 
+            config = YAML.load_file($config_file)
+        rescue
+            abort('Configuration file not found.  Exiting...')
+        end
+        File.open(File.join(File.expand_path(File.dirname(__FILE__)), 'index.html') )
+    end
+  end
+  
+  class Authenticate < R '/authenticate'
+    def get
+       @input.token = access_token
+       
     end
   end
   
   class GroupList < R '/rest/grouplist'
-	def get()
-		result = $database.execute("select * from groups")
-		headers["Content-Type"] = "application/json"
-		return result.to_json
-	end
+    def get()
+        result = $database.execute("select * from groups")
+        headers["Content-Type"] = "application/json"
+        return result.to_json
+    end
   end
 
   class WordCloud < R '/rest/wordcloud'
@@ -65,20 +73,20 @@ module GroupStats::Controllers
     
   class PostsMost < R '/rest/postsmost'
     def get()
-		if(@input.days == nil)
-			@input.days = "9999999999"
-		end
-		if(@input.groupid == nil)
-			@status = 400
-			return 'need group id'
-		end
-		
-		result = $database.execute( "SELECT user_groups.Name, count(messages.user_id) as count FROM user_groups left join messages on messages.user_id = user_groups.user_id WHERE messages.created_at > datetime('now', ?) AND messages.group_id=? AND user_groups.group_id=? group by messages.user_id order by count desc",
-		"-" + @input.days + " day",
-		@input.groupid,
+        if(@input.days == nil)
+            @input.days = "9999999999"
+        end
+        if(@input.groupid == nil)
+            @status = 400
+            return 'need group id'
+        end
+        
+        result = $database.execute( "SELECT user_groups.Name, count(messages.user_id) as count FROM user_groups left join messages on messages.user_id = user_groups.user_id WHERE messages.created_at > datetime('now', ?) AND messages.group_id=? AND user_groups.group_id=? group by messages.user_id order by count desc",
+        "-" + @input.days + " day",
+        @input.groupid,
         @input.groupid)
-		headers['Content-Type'] = "application/json"
-		return result.to_json
+        headers['Content-Type'] = "application/json"
+        return result.to_json
     end
   end
 end
