d527d3ba1a4cba40c948372d837a6fd9b65408df
wdonnell <cfonix@gmail.com>
2014-06-15 12:28:55 -0400

    moving pie chart to angular directive

--- camping/public/js/controllers.js
+++ camping/public/js/controllers.js
@@ -40,6 +40,7 @@ angular.module('myApp.controllers', [])
 	}])
 	.controller('GroupController', ['$scope', '$http', '$routeParams', function($scope, $http, $routeParams) {
 		$scope.days = 0
+		$scope.piechartData = "";
 		$scope.$watch('days', function(newValue, oldValue) {
              requestChart();
            });
@@ -52,34 +53,16 @@ angular.module('myApp.controllers', [])
 			}
 			$http({method: 'GET', url: '/rest/postsmost', params: {days : daysToRequest, groupid : $routeParams.groupid}}).
 				success(function(data, status, headers, config) {
-					drawChart(data)
+					$scope.piechartData = data
 				}).
 				error(function(data, status, headers, config) {
 
 				});
 		}
-		requestChart(); //inital load
-		
+
 		$scope.setDays = function(newday)
 		{
 			$scope.days = newday;
-		}
-		
-		//this is going in a directive soon
-		function drawChart(chartData) {
-		var data = new google.visualization.DataTable();
-		data.addColumn('string', 'Topping');
-		data.addColumn('number', 'Slices');
-		data.addRows(chartData);
-
-		// Set chart options
-		var options = {'title':'Posts in Merrifielderos by User',
-		'width':800,
-		'height':600};
-
-		// Instantiate and draw our chart, passing in some options.
-		var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
-		chart.draw(data, options);
 		}
 
 	}])
