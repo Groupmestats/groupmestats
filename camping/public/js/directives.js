447d1c95a6ba5c6e9e672a745f9806d988065e93
wdonnell <cfonix@gmail.com>
2014-06-16 18:09:58 -0400

    updates to pie chart directive

--- camping/public/js/directives.js
+++ camping/public/js/directives.js
@@ -11,38 +11,43 @@ angular.module('myApp.directives', []).
 				gmsTitle: '@'
 			},
             template: '<div id="container"></div>',	
-    		link: function (scope, element, attrs) {
-            	scope.$watch('gmsData', function(gmsData) {
+    		link: function ($scope, element, attrs) {
+            	$scope.$watch('gmsData', function(gmsData) {
 					if(gmsData)
 					{
-						drawChart(gmsData, attrs.gmsTitle, attrs.gmsWidth, attrs.gmsHeight, element[0]);
+						drawChart(gmsData, attrs.gmsTitle, element[0]);
 					}
-				})
+				});
+				
+				$scope.chart = new Highcharts.Chart({
+					chart: {
+						type: 'pie',
+						renderTo: element[0]
+					},
+					plotOptions: {
+					   pie: {
+						   allowPointSelect: true,
+						   cursor: 'pointer',
+						   dataLabels: {
+							   enabled: false
+						   },
+						   showInLegend: true
+					    }
+				    },
+					series: []
+				});
 
-				function drawChart(chartData, title, width, height, element) {
-				        $('#container').highcharts({
-                            chart: {
-                                type: 'pie'
-                            },
-                            title: {
-                                text: title
-                            },
-                            plotOptions: {
-                                           pie: {
-                                               allowPointSelect: true,
-                                               cursor: 'pointer',
-                                               dataLabels: {
-                                                   enabled: false
-                                               },
-                                               showInLegend: true
-                                           }
-                                       },
-                                   series: [{
-                                       data:
-                                           chartData
-                                   }]
-                        });
-                    }
+				function drawChart(chartData, title, element) {
+					if($scope.chart.series[0])
+					{
+						$scope.chart.series[0].remove(true);
+					}
+					$scope.chart.addSeries({
+						data: chartData
+					}, false);
+					$scope.chart.setTitle({text:title}, '', false);
+					$scope.chart.redraw();
+				}
 
 			}
 		};
