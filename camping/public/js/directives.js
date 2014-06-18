e7a48c611ecb9b0ea8f7f488fc77f295e5526532
bstascavage <brian@stascavage.com>
2014-06-18 14:03:30 -0700

    Implemented bar graphs

--- camping/public/js/directives.js
+++ camping/public/js/directives.js
@@ -139,4 +139,70 @@ angular.module('myApp.directives', []).
 
             }
         };
+    }).directive('gmsBargraph', function() {
+        return{
+            scope: {
+                gmsData: '=',
+                gmsTitle: '@'
+            },
+            template: '<div id="container"></div>',
+            link: function ($scope, element, attrs) {
+                $scope.$watch('gmsData', function(gmsData) {
+                    if(gmsData)
+                    {
+                        drawChart(gmsData, attrs.gmsTitle, element[0]);
+                    }
+                });
+
+                $scope.chart = new Highcharts.Chart({
+                    chart: {
+                        type: 'column',
+                        renderTo: element[0],
+                    },
+                    plotOptions: {
+                       line: {
+                           allowPointSelect: true,
+                           animation: {
+                               duration: 2000
+                           },
+                           cursor: 'pointer',
+                           dataLabels: {
+                               enabled: false
+                           },
+                           showInLegend: false
+                        }
+                    },
+                    xAxis: {
+                        type: 'category',
+                        title: {
+                            text: attrs.gmsXaxisTitle
+                        }
+                    },
+                    yAxis: {
+                        min: 0,
+                        title: {
+                            text: attrs.gmsYaxisTitle
+                        },
+                    },
+                    tooltip: {
+                        pointFormat: '<b>{point.y}</b><br/>',
+                        shared: true
+                    },
+                    series: []
+                });
+
+                function drawChart(chartData, title, xaxisTitle, yaxisTitle, element) {
+                    if($scope.chart.series[0])
+                    {
+                        $scope.chart.series[0].remove(true);
+                    }
+                    $scope.chart.addSeries({
+                        data: chartData
+                    }, false);
+                    $scope.chart.setTitle({text:title}, '', false);
+                    $scope.chart.redraw();
+                }
+
+            }
+        };
     });
