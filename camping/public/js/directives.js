7b29862fc5be77cfbd0e6d99264abf950cfa1cc4
bstascavage <brian@stascavage.com>
2014-07-09 12:03:30 -0700

    Fixed issue where likes piechart wasn't showing the correct info

--- camping/public/js/directives.js
+++ camping/public/js/directives.js
@@ -4,7 +4,7 @@
 
 
 angular.module('myApp.directives', []).
-	directive('gmsPiechart', function() {
+	directive('gmsPostsPiechart', function() {
         return{
             scope: {
                 gmsData: '=',
@@ -55,7 +55,136 @@ angular.module('myApp.directives', []).
                                 }
                             }
                             return '<b>'+ this.point.name + '</b>: '+ '<br>' +
-                                this.y + '<br>' + user['post_percentage'] +'%';
+                                this.y + ' posts<br>' + user['post_percentage'] +'% of posts';
+                        },
+                        shared: true
+                    },
+                    legend: {
+                        align: 'right',
+                        title: {
+                            text: attrs.gmsLegendTitle,
+                            style: {
+                                fontWeight: 'bold',
+                            }
+                        },
+                        borderColor: 'black',
+                        borderWidth: 2,
+                        layout: 'vertical',
+                        verticalAlign: 'top',
+                        y: 100,
+                        itemHoverStyle: {
+                                color: '#00f'
+                        },
+                        shadow: true
+                    },
+                    series: []
+                });
+
+                function roundToFour(num) {    
+                    return +(Math.round(num + "e+4")  + "e-4");
+                }
+
+                function userStats(userData, chart) {
+                    var user;
+                    for (var i = 0; i < userData.length; i++) {
+                            if (userData[i]['name'] == chart.series.data[chart.x].name) {
+                                user = userData[i];
+                            }
+                    }
+
+                        return '<img src="' + user.avatar_url + '" style="float:left; padding-right:15px"></img><h3>' + user.name + '</h3>' +
+                            '<br>' +
+                            '<h4>User stats:</h4>' +
+                            '<p><b>Total Posts:</b> ' + user.total_posts + '</p>' +
+                            '<p><b>Percentage of Total Posts:</b> ' + user.post_percentage + '%</p>' +
+                            '<p><b>Total likes:</b> ' + user.total_likes_received + '</p>' +
+                            '<p><b>Likes to Posts Ratio:</b> ' + user.likes_to_posts_ratio + '</p>' +
+                            '<a href="#/user?userid=' + user.user_id + '&groupid=' + user.group_id + '" onclick="javascript:parent.window.hs.close();" >More info</a>'
+                }
+
+                function drawChart(chartData, userData, title, element) {
+                    if($scope.chart.series[0])
+                    {
+                        $scope.chart.series[0].remove(true);
+                    }
+                    $scope.chart.addSeries({
+                        data: chartData,
+                        cursor: 'pointer',
+                        point: {
+                            events: {
+                                click: function(e) {
+                                    hs.htmlExpand(null, {
+                                        headingText: "User Stats",
+                                        maincontentText: userStats(userData, this)
+                                    });
+                                },
+                                legendItemClick: function(e) {
+                                    hs.htmlExpand(null, {
+                                        headingText: "User Stats",
+                                        maincontentText: userStats(userData, this)
+                                    });
+                                    return false;
+                                }
+                            }
+                        }
+                    }, false);
+                    $scope.chart.setTitle({text:title}, '', false);
+                    $scope.chart.redraw();
+                }
+            }
+        };
+    }).directive('gmsLikesPiechart', function() {
+        return{
+            scope: {
+                gmsData: '=',
+                gmsTitle: '@',
+                gmsUser: '=',
+            },
+            template: '<div id="container"></div>', 
+            link: function ($scope, element, attrs) {
+                $scope.$watch('gmsUser', function(gmsUser) {
+                    if(gmsUser)
+                    {
+                        $scope.userData = gmsUser;
+                        $scope.$watch('gmsData', function(gmsData) {
+                            if(gmsData)
+                            {
+                                $scope.data = gmsData;
+                                //drawChart(gmsData, $scope.userData, attrs.gmsTitle, element[0]);
+                            }
+                        });
+                        drawChart($scope.data, $scope.userData, attrs.gmsTitle, element[0]);
+                    }
+                });
+                
+                $scope.chart = new Highcharts.Chart({
+                    chart: {
+                        type: 'pie',
+                        renderTo: element[0],
+                    },
+                    plotOptions: {
+                       pie: {
+                           allowPointSelect: true,
+                           animation: {
+                               duration: 2000
+                           },
+                           cursor: 'pointer',
+                           dataLabels: {
+                               enabled: false
+                           },
+                           showInLegend: true
+                        },
+                    },
+                    tooltip: {
+                        formatter: function() {
+                            var user;
+                            for (var i = 0; i < $scope.userData.length; i++) {
+                                if ($scope.userData[i]['name'] == this.series.data[this.point.x].name) {
+                                    user = $scope.userData[i];
+                                }
+                            }
+                            return '<b>'+ this.point.name + '</b>: '+ '<br>' +
+                                this.y + ' likes<br>' + roundToFour(this.percentage) +'% of likes received';
                         },
                         shared: true
                     },
