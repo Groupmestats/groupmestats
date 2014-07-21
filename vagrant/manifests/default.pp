489fb540af9712fd8b27951967bdc81d2d7af66a
Brian Stascavage <bstascav@mog.home>
2014-07-20 22:36:23 -0400

    Moved vagrant to its own repo

--- vagrant/manifests/default.pp
+++
@@ -1,7 +0,0 @@
-import '/vagrant/modules/camping/manifests/init.pp'
-import '/vagrant/modules/sqlite/manifests/init.pp'
-import '/vagrant/modules/apache/manifests/init.pp'
-
-include camping
-include sqlite
-include apache
--- vagrant/modules/sqlite/manifests/init.pp
+++
@@ -1,9 +0,0 @@
-class sqlite {
-    file {'/etc/groupme.db':
-        ensure => 'present',
-        mode => 755,
-    }
-    package { 'sqlite3':
-        ensure => present,
-    }
-}
