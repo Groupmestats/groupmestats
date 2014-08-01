ea0a6390a4e897a2b9f0e69be449b97e10868239
bstascavage <brian@stascavage.com>
2014-08-01 01:39:25 +0000

    Adding some logging to scraper

--- bin/scraper.rb
+++ bin/scraper.rb
@@ -3,13 +3,15 @@ require 'rubygems'
 require 'json'
 require 'time'
 require 'sqlite3'
-require 'pp'
+require 'logger'
 
 require_relative 'groupme'
 
 #Scraper class.  This is initialized with a path to the sqlite database and a groupme oauth token
 class Scraper
+    $logger = Logger.new('/var/log/camping-server/groupstats.log')
     $time = Time.now.to_i
+    $gm = Groupme.new($logger)
 
     #Constructor that takes a path to the sqlite database and a groupme oauth token
     def initialize(database, token)
@@ -19,8 +21,11 @@ class Scraper
         begin
             database = SQLite3::Database.new( @database )
         rescue
+            $logger.error "Invalid database file"
             abort('Invalid database file')
         end
+
+        $logger.info "Database does not exist.  Creating database"
 
         if database.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='user_groups';").empty?
             database.execute('CREATE TABLE user_groups(user_id INT, group_id INT, name TEXT, FOREIGN KEY(user_id) REFERENCES users(user_id), FOREIGN KEY(group_id) REFERENCES groups(group_id));')
@@ -45,19 +50,16 @@ class Scraper
 
     #Returns the user_id
     def getUser
-        gm = Groupme.new
-        return gm.get("/users/me/", @token)['response']['id']
+        return $gm.get("/users/me/", @token)['response']['id']
     end
 
     def getUserInfo
-        gm = Groupme.new
-        return gm.get("/users/me/", @token)
+        return $gm.get("/users/me/", @token)
     end
 
     #Returns an array of hashes, containing the name and group_id of each group the user belongs to
     def getGroups
-        gm = Groupme.new
-        groups = gm.get("/groups/", @token)
+        groups = $gm.get("/groups/", @token)
         group_ids = Array.new
 
         groups['response'].each do |group|
@@ -72,18 +74,16 @@ class Scraper
 
     #Returns the last time the group was updated
     def getLastUpdate(group_id)
-        gm = Groupme.new
         database = SQLite3::Database.new( @database )
-        group = gm.get("/groups/#{group_id}/", @token)['response']
+        group = $gm.get("/groups/#{group_id}/", @token)['response']
 
         return group['updated_at']
     end
 
     #For a given group, it adds the group and users, if new.  Otherwise, it just updates them
     def populateGroup(group_id)
-        gm = Groupme.new
         database = SQLite3::Database.new( @database )
-        group = gm.get("/groups/#{group_id}/", @token)['response']
+        group = $gm.get("/groups/#{group_id}/", @token)['response']
 
         if group['image_url'].nil?
             group['image_url'] = 'img/groupme.png'
@@ -100,6 +100,7 @@ class Scraper
         #Adds new group if they don't exist, and updates the group if they do 
         if database.execute( "SELECT * FROM groups WHERE group_id='#{group['group_id']}'").empty? 
             group_info[group['group_id']] = [group['created_at'], group['name'], group['image_url'], group['creator_user_id'], false ]
+            $logger.info "Adding new group #{group['name']} to the database"
         else
             group_info[group['group_id']] = [group['created_at'], group['name'], group['image_url'], group['creator_user_id'], true ]
         end
@@ -170,7 +171,6 @@ class Scraper
     end
 
     def scrapeNewMessages(group_id)
-        gm = Groupme.new
         database = SQLite3::Database.new( @database )
 
         if database.execute( "SELECT * FROM groups WHERE group_id='#{group_id}'").empty?
@@ -180,14 +180,16 @@ class Scraper
             update_time = database.execute( "SELECT strftime('%s', updated_at) FROM groups WHERE group_id='#{group_id}'" )        
             scrapeMessages(Time.now.to_i - update_time[0][0].to_i, group_id)
         end
-        group = gm.get("groups/#{group_id}/", @token)['response']
+        
+        group = $gm.get("groups/#{group_id}/", @token)['response']
+        $logger.info "Scraped messages from #{group['name']}"
+
         database = SQLite3::Database.new( @database )
         database.execute("UPDATE groups SET updated_at=datetime('#{group['updated_at']}','unixepoch') WHERE group_id='#{group['group_id']}'")
     end
 
     #This will pull all messages for a given period of time and group, and store it in the messages table.  Takes in a time, in epoch seconds.  To search for all messages, entire the current epoch time.
     def scrapeMessages(searchTime = Time.now.to_i, group_id)
-        gm = Groupme.new
         database = SQLite3::Database.new( @database ) 
 
         id = 0
@@ -196,12 +198,14 @@ class Scraper
         scraped_messages = Hash.new        
         scraped_likes = Array.new
 
+        t1 = Time.new
+
         while (Time.now.to_i - t.to_i) < (searchTime + 604800) do
 
             if id == 0
-                messages = gm.get("groups/#{group_id}/messages", @token, "limit=100")['response']
+                messages = $gm.get("groups/#{group_id}/messages", @token, "limit=100")['response']
             else
-                messages = gm.get("groups/#{group_id}/messages", @token, "limit=100&before_id=#{id}")['response']
+                messages = $gm.get("groups/#{group_id}/messages", @token, "limit=100&before_id=#{id}")['response']
             end
 
             if messages.nil?
@@ -265,8 +269,10 @@ class Scraper
                 likes[0],
                 likes[1] )
         end 
-        pp "TESTING"       
         database.commit
+        
+        t2 = Time.new
+        $logger.info "Scrape time for group id #{group_id} was: #{(t2-t1).to_s} seconds"
     end
 
     private :scrapeMessages
