8a826a5b10209c8262ee74b93248ee1250b58837
bstascavage <brian@stascavage.com>
2014-08-13 03:38:24 +0000

    Added heatmap to "at a glance"

--- camping/public/js/controllers.js
+++ camping/public/js/controllers.js
@@ -233,8 +233,10 @@ angular.module('myApp.controllers', [])
 	}])
     .controller('GlanceController', ['$scope', '$http', '$routeParams', function($scope, $http, $routeParams) {
         //var user = $scope.user[index]
+	var offset = new Date().getTimezoneOffset();
+        offset = offset / 60;
 
-        $http({method: 'GET', url: '/rest/user'}).
+        $http({method: 'GET', url: '/rest/user', params: {timezone: offset}}).
                 success(function(data, status, headers, config) {
                     $scope.user = data
                 }).
