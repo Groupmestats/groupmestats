69353b8becaca5aa7710e715873665553d259a75
bstascavage <brian@stascavage.com>
2015-07-17 23:05:14 +0000

    Added initial time selectors

--- camping/public/js/controllers.js
+++ camping/public/js/controllers.js
@@ -4,28 +4,11 @@
 
 angular.module('gmStats.controllers', [])
 	.controller('GroupListController', ['$scope', '$http', '$routeParams', '$location', function($scope, $http, $routeParams, $location) {
-		$scope.scraping = false;
-		
 		$scope.setScraping = function(groupId)
 		{
-			$scope.scraping = true;
 			$location.path("/group").search({groupid: groupId})
 		}
 		
-		$scope.scrapeGroup = function(index)
-		{
-			var group = $scope.groupList[index]
-			group.loading = true;
-			$http({method: 'GET', url: '/rest/scrapegroup', params: { groupid: group.group_id }}).
-				success(function(data, status, headers, config) {
-					group.loading = false;
-					//todo: refresh last updated date
-				}).
-				error(function(data, status, headers, config) {
-					group.loading = false;
-				});
-		}
-	
 		$http({method: 'GET', url: '/rest/groupList'}).
 				success(function(data, status, headers, config) {
 					$scope.groupList = data
@@ -44,14 +27,6 @@ angular.module('gmStats.controllers', [])
                   $scope.groupListLoading = false;
               });
        }
-       $scope.scrapeAll = function()
-       {
-          $http({method: 'GET', url: '/rest/scrapeall'}).
-              success(function(data, status, headers, config) {
-              }).
-              error(function(data, status, headers, config) {
-              });
-       }
 }])
 	.controller('GroupController', ['$scope', '$http', '$routeParams', '$sce', function($scope, $http, $routeParams, $sce) {
 		$scope.days = 0
@@ -60,14 +35,19 @@ angular.module('gmStats.controllers', [])
 
 		$http({method: 'GET', url: '/rest/groupfacts', params: {groupid : $routeParams.groupid}}).
 			success(function(data, status, headers, config) {
-					$scope.groupfacts = data
-					$scope.groupfactsGroupId = $sce.trustAsResourceUrl($scope.groupfacts.group_id);
-					$scope.groupurl = "https://felannisport.com/kibana/#/dashboard/Groupmestats?embed&_g=(refreshInterval:(display:Off,section:0,value:0),time:(from:now-2y,mode:quick,to:now))&_a=(filters:!(),panels:!((col:1,id:'Groupmestats:-Messages-over-time-by-User',row:13,size_x:12,size_y:6,type:visualization),(col:1,id:'Groupmestats:-Posts-per-User',row:3,size_x:5,size_y:5,type:visualization),(col:4,id:'Groupmestats:-Average-Likes-per-post',row:1,size_x:3,size_y:2,type:visualization),(col:1,id:'Groupmestats:-Total-Posts',row:1,size_x:3,size_y:2,type:visualization),(col:1,id:'Groupmestats:-Most-liked-Images',row:8,size_x:6,size_y:5,type:search),(col:6,id:'Groupmestats:-Most-liked-Posts',row:3,size_x:7,size_y:5,type:search),(col:10,id:'Groupmestats:-Total-Images',row:1,size_x:3,size_y:2,type:visualization),(col:7,id:'Groupmestats:-Active-Users',row:1,size_x:3,size_y:2,type:visualization),(col:7,id:'Groupmestats:-Unique-users-posting-over-time',row:8,size_x:6,size_y:5,type:visualization)),query:(query_string:(analyze_wildcard:!t,query:group_id%3D" + data.group_id + ")),title:Groupmestats)"
+					$scope.group = data
+					$scope.groupurl = "https://192.168.4.50/kibana/#/dashboard/Groupmestats?embed&_g=(refreshInterval:(display:Off,section:0,value:0),time:(from:now-2y,mode:quick,to:now))&_a=(filters:!(),panels:!((col:1,id:'Groupmestats:-Messages-over-time-by-User',row:13,size_x:12,size_y:6,type:visualization),(col:1,id:'Groupmestats:-Posts-per-User',row:3,size_x:5,size_y:5,type:visualization),(col:4,id:'Groupmestats:-Average-Likes-per-post',row:1,size_x:3,size_y:2,type:visualization),(col:1,id:'Groupmestats:-Total-Posts',row:1,size_x:3,size_y:2,type:visualization),(col:1,id:'Groupmestats:-Most-liked-Images',row:8,size_x:6,size_y:5,type:search),(col:6,id:'Groupmestats:-Most-liked-Posts',row:3,size_x:7,size_y:5,type:search),(col:10,id:'Groupmestats:-Total-Images',row:1,size_x:3,size_y:2,type:visualization),(col:7,id:'Groupmestats:-Active-Users',row:1,size_x:3,size_y:2,type:visualization),(col:7,id:'Groupmestats:-Unique-users-posting-over-time',row:8,size_x:6,size_y:5,type:visualization)),query:(query_string:(analyze_wildcard:!t,query:group_id%3D" + data.group_id + ")),title:Groupmestats)"
 					$scope.groupfactsUrl = $sce.trustAsResourceUrl($scope.groupurl);
 			}).
 			error(function(data, status, headers, config) {
 
 			});
+		
+		$scope.setDays = function(newday)
+		{
+			 $scope.groupurl = "https://192.168.4.50/kibana/#/dashboard/Groupmestats?embed&_g=(refreshInterval:(display:Off,section:0,value:0),time:(from:now-" + newday + ",mode:quick,to:now))&_a=(filters:!(),panels:!((col:1,id:'Groupmestats:-Messages-over-time-by-User',row:13,size_x:12,size_y:6,type:visualization),(col:1,id:'Groupmestats:-Posts-per-User',row:3,size_x:5,size_y:5,type:visualization),(col:4,id:'Groupmestats:-Average-Likes-per-post',row:1,size_x:3,size_y:2,type:visualization),(col:1,id:'Groupmestats:-Total-Posts',row:1,size_x:3,size_y:2,type:visualization),(col:1,id:'Groupmestats:-Most-liked-Images',row:8,size_x:6,size_y:5,type:search),(col:6,id:'Groupmestats:-Most-liked-Posts',row:3,size_x:7,size_y:5,type:search),(col:10,id:'Groupmestats:-Total-Images',row:1,size_x:3,size_y:2,type:visualization),(col:7,id:'Groupmestats:-Active-Users',row:1,size_x:3,size_y:2,type:visualization),(col:7,id:'Groupmestats:-Unique-users-posting-over-time',row:8,size_x:6,size_y:5,type:visualization)),query:(query_string:(analyze_wildcard:!t,query:group_id%3D" + $scope.group.group_id + ")),title:Groupmestats)"
+                         $scope.groupfactsUrl = $sce.trustAsResourceUrl($scope.groupurl);
+		}
 	}])
 	.controller('AboutController', ['$scope', function($scope) {
 
